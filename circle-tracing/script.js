646c508d67f0c8b217dc8a5cc1a3c76c5df3f330
kryseno <kryseno@gmail.com>
2017-11-24 02:14:45 -0800

    [added] functionality for placing target onto random squares. currently trying to figure out functionality for if div doesn't have circle

--- circle-tracing/script.js
+++ circle-tracing/script.js
@@ -1,9 +1,18 @@
 $(document).ready(initializeApp);
 
+// var squares = $(".square");
+// var div = $("div");
+
 function initializeApp(){
     $(".square").click(handleSquareCLick);
 }
 
 function handleSquareCLick(){
     console.log('square has been clicked', this);
+    if($(".square").children().length < 1){
+        return
+    } else {
+        var randomSquare = $(".square").eq(Math.floor(Math.random()*3) + 1);
+        $("#target").prependTo(randomSquare);
+    }
 }
