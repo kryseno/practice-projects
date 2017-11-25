9e8cdf97428a0bdef0b305952e65e1be5033855c
kryseno <kryseno@gmail.com>
2017-11-25 00:42:34 -0800

    [disregard] functionality for decrement working. actually, increments both divs. [moved] decrementText var declaration to beginning of handleDivClicks function

--- counter-transfer/script.js
+++ counter-transfer/script.js
@@ -10,17 +10,16 @@ function initializeApp(){
 function handleDivClicks(){
     console.log('div clicks enabled');
     var incrementText = $(this).text();
+    var decrementText = $(this).text();
     console.log('this is: ',this);
     if($(this).hasClass('counter1')){
         incrementText++;
-        $(this).text(incrementText);
-        decrementText = $(this).text(); 
+        $(this).text(incrementText); 
         decrementText--;       
         $(".counter2").text(decrementText);
     } else {
         incrementText++;
-        $(this).text(incrementText);
-        decrementText = $(this).text(); 
+        $(this).text(incrementText); 
         decrementText--;       
         $(".counter1").text(decrementText);
     }
