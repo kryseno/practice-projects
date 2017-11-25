4e56fc1af8f1619f3b1d60e1053fa020496c0e28
kryseno <kryseno@gmail.com>
2017-11-25 00:36:56 -0800

    [added] div 1 functionality for incrementing current div clicked & decrementing other div. figure out how to decrement once number increments. right now, it decrements after one click.

--- counter-transfer/script.js
+++ counter-transfer/script.js
@@ -6,3 +6,16 @@ function initializeApp(){
     console.log('app initialized');
     $("div").click(handleDivClicks);
 }
+
+function handleDivClicks(){
+    console.log('div clicks enabled');
+    var incrementText = $(this).text();
+    console.log('this is: ',this);
+    if($(this).hasClass('counter1')){
+        incrementText++;
+        $(this).text(incrementText);
+        decrementText = $(this).text(); 
+        decrementText--;       
+        $(".counter2").text(decrementText);
+    }
+}
