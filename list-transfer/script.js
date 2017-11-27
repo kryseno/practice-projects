70a22404778c99816cf53fdbcb4251f389432f67
kryseno <kryseno@gmail.com>
2017-11-26 17:50:49 -0800

    [added] initializeApp, clickhandler to divs

---
+++ list-transfer/script.js
@@ -0,0 +1,14 @@
+$(document).ready(initializeApp);
+
+function initializeApp(){
+    $("div").click(handleDivClick);
+}
+
+function handleDivClick(){
+    var lastListItem = 
+    console.log('div clicked');
+}
+
+// 1. make both divs clickable
+// 2. save last li into var
+// 3. append that li into ul that DOES NOT have class (this class)
